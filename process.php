ef927bebae1ac75fa7185f5f101d2c75f038831c
JPeng0 <pengx205@umn.edu>
2017-07-13 00:53:27 -0500

    Updated with working php file

--- process.php
+++ process.php
@@ -1,41 +1,31 @@
 <?php
 // process.php
-
-$errors         = array();      // array to hold validation errors
-$data           = array();      // array to pass back data
-
+$data = false;
 // validate the variables ======================================================
 // if any of these variables don't exist, add an error to our $errors array
 
-if (empty($_POST['name']))
-{$errors['name'] = 'Name is required.';}
-    
-if (empty($_POST['email']))
-{$errors['email'] = 'Email is required.';}
+if (empty($_POST['Message']))
+	$errors['message'] = 'Message is required.';
+
         
-            
             // return a response ===========================================================
             
             // if there are any errors in our errors array, return a success boolean of false
- if ( ! empty($errors)) {
-                
-                // if there are items in our errors array, return those errors
-                $data['success'] = false;
-                $data['errors']  = $errors;
-            } else {
-                
+if ( ! empty($errors)) {
+	$data = false;
+}
                 // if there are no errors process our form, then return a message
-                
+                else {
                 $name = $_POST['name'];
   				$email = $_POST['email'];
   				$message = $_POST['message'];
-  				$subject = "Sent by " . $name. " from web form";
-  				$header = "From:" . $email;	
-                mail("pengx205@umn.edu", $subject, $message, $header);
+  				$subject = 'Sent by ' . $name. ' from web form';
+  				$header = 'From:' . $email;	
+                mail('pengx205@umn.edu', $subject, $message, $header);
                 // show a message of success and provide a true success variable
-                $data['success'] = true;
-                $data['message'] = 'Success!';
-            }
+                $data = true;
+                }
+            
             
             // return all our data to an AJAX call
             echo json_encode($data);
